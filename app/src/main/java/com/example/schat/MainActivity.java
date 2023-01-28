57085b72996445dda7dd681bd1f076c011c27353
sharkz <mosenjemudogoz@gmail.com>
2023-01-28 14:26:57 +0300

    bottom nav bar

--- app/src/main/java/com/example/schat/MainActivity.java
+++ app/src/main/java/com/example/schat/MainActivity.java
@@ -1,14 +1,43 @@
 package com.example.schat;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
-
+    BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView=findViewById(R.id.btmNavigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.home:
+                        Toast.makeText(MainActivity.this, "Home Clicked", Toast.LENGTH_SHORT).show();
+                        return true;
+                    case R.id.people:
+                        Toast.makeText(MainActivity.this, "Users Clicked", Toast.LENGTH_SHORT).show();
+                        return true;
+                    case R.id.call:
+                        Toast.makeText(MainActivity.this, "call Clicked", Toast.LENGTH_SHORT).show();
+                        return true;
+                    case R.id.settings:
+                        Toast.makeText(MainActivity.this, "Setting Clicked", Toast.LENGTH_SHORT).show();
+                        return true;
+                }
+                return false;
+            }
+        });
+
+
     }
 }
