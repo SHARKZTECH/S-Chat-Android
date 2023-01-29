f15d37f10b8c8a748a5c0c725eb3c0bb9f5be62e
sharkz <mosenjemudogoz@gmail.com>
2023-01-29 05:14:55 +0300

    chat layout

--- app/src/main/java/com/example/schat/ChatActivity.java
+++ app/src/main/java/com/example/schat/ChatActivity.java
@@ -1,14 +1,26 @@
 package com.example.schat;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 public class ChatActivity extends AppCompatActivity {
 
+    Toolbar toolbar;
+    @SuppressLint("RestrictedApi")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
+        toolbar=findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDefaultDisplayHomeAsUpEnabled(true);
+
+
     }
 }
