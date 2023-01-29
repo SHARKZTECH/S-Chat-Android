058472b31c1e981e9a72869d448de4e843dcdad6
sharkz <mosenjemudogoz@gmail.com>
2023-01-29 17:04:38 +0300

    home fragment

--- app/src/main/java/com/example/schat/ChatActivity.java
+++ app/src/main/java/com/example/schat/ChatActivity.java
@@ -1,16 +1,13 @@
 package com.example.schat;
 
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 public class ChatActivity extends AppCompatActivity {
 
     Toolbar toolbar;
-    @SuppressLint("RestrictedApi")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,8 +15,8 @@ public class ChatActivity extends AppCompatActivity {
 
         toolbar=findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        getSupportActionBar().setDefaultDisplayHomeAsUpEnabled(true);
 
 
     }
