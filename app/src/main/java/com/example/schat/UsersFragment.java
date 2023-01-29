a92a2e9749d1e884ac78e42bd24811f3b75bfdc3
sharkz <mosenjemudogoz@gmail.com>
2023-01-29 04:18:46 +0300

    chat layout

--- app/src/main/java/com/example/schat/UsersFragment.java
+++ app/src/main/java/com/example/schat/UsersFragment.java
@@ -1,5 +1,6 @@
 package com.example.schat;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +8,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 
 public class UsersFragment extends Fragment {
@@ -14,7 +17,18 @@ public class UsersFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_users, container, false);
+        View view= inflater.inflate(R.layout.fragment_users, container, false);
+
+        Button button=view.findViewById(R.id.chatBtn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getContext(),ChatActivity.class));
+            }
+        });
+
+
+
+        return view;
     }
 }
