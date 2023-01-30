bacd4a0938a32bca2e59de47ae55cd41e7457dad
sharkz <mosenjemudogoz@gmail.com>
2023-01-30 06:16:44 +0300

    user fragment

---
+++ app/src/main/java/com/example/schat/UsersAdapter.java
@@ -0,0 +1,35 @@
+package com.example.schat;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class UsersAdapter extends RecyclerView.Adapter<UsersAdapter.MyViewHolder> {
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.user_item,parent,false);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 4;
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder{
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
--- app/src/main/java/com/example/schat/UsersFragment.java
+++ app/src/main/java/com/example/schat/UsersFragment.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,20 +16,27 @@ import android.widget.Toast;
 
 public class UsersFragment extends Fragment {
 
+    RecyclerView recyclerView;
+    Button button;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view= inflater.inflate(R.layout.fragment_users, container, false);
 
-        Button button=view.findViewById(R.id.chatBtn);
+        button=view.findViewById(R.id.chatBtn);
+       recyclerView=view.findViewById(R.id.recycler);
+
+
+        UsersAdapter usersAdapter=new UsersAdapter();
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.setAdapter(usersAdapter);
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(getContext(),ChatActivity.class));
             }
         });
-
-
 
         return view;
     }
