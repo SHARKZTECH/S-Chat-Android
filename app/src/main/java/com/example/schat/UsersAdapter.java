0260a356778f0645baa7efb6819c1f076aeac0c9
sharkz <mosenjemudogoz@gmail.com>
2023-02-07 00:34:39 +0300

    last msg

--- app/src/main/java/com/example/schat/UsersAdapter.java
+++ app/src/main/java/com/example/schat/UsersAdapter.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Build;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,6 +14,11 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
 
 import java.util.List;
@@ -52,6 +58,30 @@ public class UsersAdapter extends RecyclerView.Adapter<UsersAdapter.MyViewHolder
             holder.onlineStatus.setBackground(context.getResources().getDrawable(R.drawable.bg_white));
         }
 
+        FirebaseDatabase.getInstance().getReference("chats")
+                .child(FirebaseAuth.getInstance().getUid()+ user.getUserId())
+                        .orderByChild("createdAt").limitToLast(1)
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        if (snapshot.hasChildren()){
+                            for (DataSnapshot snapshot1:snapshot.getChildren()) {
+                                Message message=snapshot1.getValue(Message.class);
+                                holder.lastMsg.setText(message.getText());
+                                if(message.getuId().equals(FirebaseAuth.getInstance().getUid()) && user.getStatus().equals("online")){
+                                    holder.lastMsgIcon.setVisibility(View.VISIBLE);
+                                }
+                                notifyDataSetChanged();
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+
         holder.itemView.setOnClickListener(view -> {
             Intent intent=new Intent(context,ChatActivity.class);
             intent.putExtra("user",user);
@@ -66,7 +96,7 @@ public class UsersAdapter extends RecyclerView.Adapter<UsersAdapter.MyViewHolder
 
     public class MyViewHolder extends RecyclerView.ViewHolder{
         TextView userName,lastMsg,msgCount,time,onlineStatus;
-        ImageView imageView;
+        ImageView imageView,lastMsgIcon;
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
             userName=itemView.findViewById(R.id.userName);
@@ -75,6 +105,7 @@ public class UsersAdapter extends RecyclerView.Adapter<UsersAdapter.MyViewHolder
             time=itemView.findViewById(R.id.time);
             onlineStatus=itemView.findViewById(R.id.onlineStatus);
             imageView=itemView.findViewById(R.id.userImg);
+            lastMsgIcon=itemView.findViewById(R.id.lastMassageIcon);
         }
     }
 }
