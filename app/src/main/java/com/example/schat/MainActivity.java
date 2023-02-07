e7cc8e53f0e170035f5bfa0658f22043f15d898e
sharkz <mosenjemudogoz@gmail.com>
2023-02-07 18:56:03 +0300

    page viewer

--- app/src/main/java/com/example/schat/MainActivity.java
+++ app/src/main/java/com/example/schat/MainActivity.java
@@ -2,10 +2,15 @@ package com.example.schat;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.MenuItem;
+import android.widget.AdapterView;
 import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -22,6 +27,7 @@ import java.util.Map;
 
 public class MainActivity extends AppCompatActivity implements HomeFragment.HomeFrag{
     BottomNavigationView bottomNavigationView;
+    ViewPager viewPager;
     FirebaseDatabase fDb;
     DatabaseReference dbRef;
     @Override
@@ -30,35 +36,41 @@ public class MainActivity extends AppCompatActivity implements HomeFragment.Home
         setContentView(R.layout.activity_main);
 
         bottomNavigationView=findViewById(R.id.btmNavigation);
+        viewPager=findViewById(R.id.pager);
 
         fDb=FirebaseDatabase.getInstance();
         dbRef=fDb.getReference("users").child(FirebaseAuth.getInstance().getUid());
 
 
-        UsersFragment usersFragment=new UsersFragment();
-        SettingsFragment settingsFragment=new SettingsFragment();
-        CallFragment callFragment=new CallFragment();
-        HomeFragment homeFragment=new HomeFragment();
+//        UsersFragment usersFragment=new UsersFragment();
+//        SettingsFragment settingsFragment=new SettingsFragment();
+//        CallFragment callFragment=new CallFragment();
+//        HomeFragment homeFragment=new HomeFragment();
 
-        bottomNavigationView.setSelectedItemId(R.id.home);
-        getSupportFragmentManager().beginTransaction().add(R.id.container,homeFragment).commit();
+//        bottomNavigationView.setSelectedItemId(R.id.home);
+//        getSupportFragmentManager().beginTransaction().add(R.id.container,homeFragment).commit();
 
-
+        BottomPagerAdapter adapter=new BottomPagerAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(adapter);
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch (item.getItemId()){
                     case R.id.home:
-                        getSupportFragmentManager().beginTransaction().replace(R.id.container,homeFragment).commit();
+                        viewPager.setCurrentItem(0);
+//                        getSupportFragmentManager().beginTransaction().replace(R.id.container,homeFragment).commit();
                         return true;
                     case R.id.people:
-                        getSupportFragmentManager().beginTransaction().replace(R.id.container,usersFragment).commit();
+                        viewPager.setCurrentItem(1);
+//                        getSupportFragmentManager().beginTransaction().replace(R.id.container,usersFragment).commit();
                         return true;
                     case R.id.call:
-                        getSupportFragmentManager().beginTransaction().replace(R.id.container,callFragment).commit();
+                        viewPager.setCurrentItem(2);
+//                        getSupportFragmentManager().beginTransaction().replace(R.id.container,callFragment).commit();
                         return true;
                     case R.id.settings:
-                        getSupportFragmentManager().beginTransaction().replace(R.id.container,settingsFragment).commit();
+                        viewPager.setCurrentItem(3);
+//                        getSupportFragmentManager().beginTransaction().replace(R.id.container,settingsFragment).commit();
                         return true;
                 }
                 return false;
@@ -99,9 +111,34 @@ public class MainActivity extends AppCompatActivity implements HomeFragment.Home
 
     @Override
     public void onClick() {
-        UsersFragment usersFragment=new UsersFragment();
-        getSupportFragmentManager().beginTransaction()
-                .replace(R.id.container,usersFragment).commit();
+//        UsersFragment usersFragment=new UsersFragment();
+//        getSupportFragmentManager().beginTransaction()
+//                .replace(R.id.container,usersFragment).commit();
+        viewPager.setCurrentItem(1);
         bottomNavigationView.setSelectedItemId(R.id.people);
+    }
+
+    public class BottomPagerAdapter  extends FragmentPagerAdapter {
+
+        public BottomPagerAdapter(@NonNull FragmentManager fm) {
+            super(fm);
+        }
+
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            switch (position){
+                case 0: return new HomeFragment();
+                case 1:return new UsersFragment();
+                case 2:return new CallFragment();
+                case 3:return new SettingsFragment();
+            }
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            return 4;
+        }
     }
 }
